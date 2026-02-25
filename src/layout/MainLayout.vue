d76f26486f6fffc2ede01b297b92c0340e894474
Gonzalo Moreno <gonzalomd162@gmail.com>
2026-02-25 18:57:10 +0100

    scroll to section hecho, faltan estilos y organizacion

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -5,9 +5,9 @@ import { RouterLink } from "vue-router";
 
 
 <template>
-    <div class="flex justify-center gap-5 pr-10 pt-6 pb-6 bg-slate-800  z-50  ">
-        <nav  class="flex items-center gap-6 h-10 px-6 py-2 rounded-full bg-white/25 backdrop-blur-md ">
-             <RouterLink to="/home"
+    <div class=" bg-[#04090ffe] flex justify-center gap-5 pr-10 pt-10 pb-6    ">
+        <div class="flex items-center gap-6 h-10 px-6 py-2 rounded-full bg-white/25 backdrop-blur-md ">
+             <RouterLink to="/"
             class= " flex px-4 items-center rounded-full hover:bg-white/30 transition">
                 Home
             </RouterLink>
@@ -26,7 +26,7 @@ import { RouterLink } from "vue-router";
            
                 Contact
             </RouterLink>
-        </nav>
+        </div>
              
 
     </div>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,5 +1,43 @@
 <script lang="ts" setup>
+import { scrollToSection } from '@/utils/scrollToSection';
+import {
+  NavigationMenu,
+ 
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  
+  navigationMenuTriggerStyle,
+  
+} from '@/components/ui/navigation-menu'
 
+
+
+
+const menuItems = [
+  {
+    label:'Home',
+    href: '#',
+    onClick: () => scrollToSection('#')
+  },
+    {
+    label:'3D',
+    href: '#3d',
+    onClick: () => scrollToSection('#vehiculis')
+  },  {
+    label:'Conecta',
+    href: '#conecta',
+    onClick: () => scrollToSection('#videre')
+  },  {
+    label:'Carteles',
+    href: '#carteles',
+    onClick: () => scrollToSection('#contactus')
+  },{
+    label:'About',
+    href: '#about',
+    onClick: () => scrollToSection('#contactus')
+  },
+]
 
 
 
@@ -9,21 +47,76 @@
 
 <template>
 
-<div class="bg-slate-800">
+<section id="home">
+  <div class="bg-[#04090ffe]  ">
+    
     <img 
-            src="/RenderFinalPelotiña-mas-pequeña.jpg"
+            src="/PelotaPortada_fondoGrande.jpg"
             alt="Imagen principal"
             class="pt-3.5 w-screen  object-cover "
-  />
-       
-        <img
-            src="/Arma render 1.jpg"
-            alt="Imagen principal"
-            class="pt-3.5 w-screen  object-cover"
-  />
-    <div ></div>
+    />
+  
     </div>
 
+</section>
+<section id="3D">
+  <div class="bg-[#04090ffe]  ">
+    
+    <img 
+            src="/PelotaPortada_fondoGrande.jpg"
+            alt="Imagen principal"
+            class="pt-3.5 w-screen  object-cover "
+    />
+  
+    </div>
+
+</section>
+
+ <NavigationMenu>
+    <NavigationMenuList>
+
+      <NavigationMenuItem>
+        <a href="#" @click.prevent="scrollToSection('#')">
+          <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+            Home
+          </NavigationMenuLink> 
+        </a>
+      </NavigationMenuItem>
+
+      <NavigationMenuItem>
+        <a href="#" @click.prevent="scrollToSection('#3D')">
+          <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+            3D
+          </NavigationMenuLink> 
+        </a>
+      </NavigationMenuItem>
+
+      <NavigationMenuItem>
+        <a href="#" @click.prevent="scrollToSection('#conecta')">
+          <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+            Conecta
+          </NavigationMenuLink> 
+        </a>
+      </NavigationMenuItem>
+
+      <NavigationMenuItem>
+        <a href="#" @click.prevent="scrollToSection('#carteles')">
+          <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+            Carteles
+          </NavigationMenuLink> 
+        </a>
+      </NavigationMenuItem>
+
+      <NavigationMenuItem>
+        <a href="#" @click.prevent="scrollToSection('#contacto')">
+          <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+            Contacto
+          </NavigationMenuLink> 
+        </a>
+      </NavigationMenuItem>
+
+    </NavigationMenuList>
+  </NavigationMenu>
         
 
 </template>
